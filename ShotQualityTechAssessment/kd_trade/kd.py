4f96031034d8d07a3cada30e3f854f30ed89b66f
gusunny29 <53237770+gusunny29@users.noreply.github.com>
2024-09-30 03:34:44 -0400

    Fall Technical Done, KD started, created files for rest

--- ShotQualityTechAssessment/kd_trade/kd.py
+++ ShotQualityTechAssessment/kd_trade/kd.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sklearn.linear_model as LinearRegression
 
 csv_file = "nba_data_modeling_engineer.csv"
 
@@ -13,15 +14,45 @@ filtered_data = []
 # focus on play data of these teams
 for chunk in pd.read_csv(csv_file, chunksize=chunk_size):
     # Filter for rows where the team is either 'Knicks' or 'Nets'
+    team_columns = chunk.columns[chunk.columns.str.contains("Team", case=False)]
+    player_team_columns = chunk.columns[
+        chunk.columns.str.contains("teamPlayer1", case=False)
+    ]
+
+    # Filter for rows where any of the team columns contain 'NYK' or 'BKN'
     filtered_chunk = chunk[
-        (chunk["slugTeamHome"] == "NYK")
-        | (chunk["slugTeamHome"] == "BKN")
-        | (chunk["slugTeamAway"] == "NYK")
-        | (chunk["slugTeamAway"] == "BKN")
+        (
+            chunk[team_columns].apply(
+                lambda col: col.astype(str).str.contains("NYK")
+                | col.astype(str).str.contains("BKN")
+            )
+        ).any(axis=1)
+    ]
+    # filtered_chunk_with_score = filtered_chunk[
+    #     filtered_chunk["slugScore"].apply(lambda x: isinstance(x, str))
+    # ]
+
+    # double check
+    filter_no_players = filtered_chunk[
+        filtered_chunk["namePlayer1"].apply(lambda x: isinstance(x, str))
+    ]
+
+    # Examine misses
+    filter_knicks_nets_shooting = filter_no_players[
+        (
+            chunk[player_team_columns].apply(
+                lambda col: col.astype(str).str.contains("NYK")
+                | col.astype(str).str.contains("BKN")
+            )
+        ).any(axis=1)
+    ]
+
+    filtered_chunks_shooting_events = filter_knicks_nets_shooting[
+        filter_knicks_nets_shooting["numberEvent"] > 2
     ]
 
     # Append the filtered chunk to the list
-    filtered_data.append(filtered_chunk)
+    filtered_data.append(filtered_chunks_shooting_events)
 
 filtered_df = pd.concat(filtered_data)
 
